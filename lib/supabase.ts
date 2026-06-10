83fb264a7d3d76e3229d08114cfe7abeda6a5801
Beckett Strong <beckettstrong@becketts-air.lan>
2026-06-09 20:35:07 -0600

    restore styles object

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("KEY EXISTS:", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
